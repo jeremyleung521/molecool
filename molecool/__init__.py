27f390c52941c2971ee44b9e63b1ba4b60fbcca5
Jeremy Leung <jeremyleung521@gmail.com>
2020-05-15 15:25:40 -0400

    fixed __init__.py

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -9,6 +9,7 @@ from .molecule import build_bond_list
 from .measure import calculate_distance, calculate_angle
 from .visualize import draw_molecule, bond_histogram
 from .atom_data import atomic_weights, atom_colors
+from .io import open_pdb, open_xyz, write_xyz
 #import molecool.functions
 
 # Handle versioneer
